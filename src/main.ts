3251b48d279dfaedf91c982004320beeaba0f071
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2023-11-27 02:04:17 -0600

    feat: visualizar mapa

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+Mapboxgl.accessToken = 'pk.eyJ1IjoibWV0YWwxOTkwIiwiYSI6ImNsbGczdjVmNzBxY2EzZXFhbDFtamRuazMifQ.UVGCqlR5xXEuQGmamcet3w';
+
 if ( !navigator.geolocation ) {
   alert( 'Navegador no soprta el Geolocation' );
   throw new Error('Navegador no soprta el Geolocation')
